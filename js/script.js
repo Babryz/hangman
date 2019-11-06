2774accc8016afa8936d8ff684bd7802a8544882
Emil Babra <brittsboemil@gmail.com>
2019-11-06 16:11:48 +0100

    Connect variables to right elements and start making functions.

--- js/script.js
+++ js/script.js
@@ -1,19 +1,50 @@
 // Globala variabler
 
-const wordList;      // Array: med spelets alla ord
+const wordList = ['hangman', 'develop', 'anomaly', 'manhunt', 'human', 'dinosaur', ''];      // Array: med spelets alla ord
 let selectedWord;    // Sträng: ett av orden valt av en slumpgenerator från arrayen ovan
 
 let guesses = 0;     // Number: håller antalet gissningar som gjorts
 let hangmanImg;      // Sträng: sökväg till bild som kommer visas (och ändras) fel svar. t.ex. `/images/h1.png`
 
-let msgHolderEl;     // DOM-nod: Ger meddelande när spelet är över
-let startGameBtnEl;  // DOM-nod: knappen som du startar spelet med
-let letterButtonEls; // Array av DOM-noder: Knapparna för bokstäverna
-let letterBoxEls;    // Array av DOM-noder: Rutorna där bokstäverna ska stå
+let msgHolderEl = document.getElementById('message');     // DOM-nod: Ger meddelande när spelet är över
+let startGameBtnEl = document.getElementById('startGameBtn');  // DOM-nod: knappen som du startar spelet med
+let letterButtonEls = document.querySelectorAll('#letterButtons > li > button'); // Array av DOM-noder: Knapparna för bokstäverna
+let letterBoxEls = document.querySelectorAll('#letterBoxes > ul > li > input');    // Array av DOM-noder: Rutorna där bokstäverna ska stå
 
-// Funktion som startar spelet vid knapptryckning, och då tillkallas andra funktioner
-// Funktion som slumpar fram ett ord
-// Funktion som tar fram bokstävernas rutor, antal rutor beror på vilket ord slumptas fram
-// Funktion som körs när du trycker på bokstäverna och gissar bokstav
-// Funktion som ropas vid vinst eller förlust, gör olika saker beroende tillståndet
-// Funktion som inaktiverar/aktiverar bokstavsknapparna beroende på vilken del av spelet du är på
+function startGame() {
+    generateRandomWord();
+    
+}
+
+function generateRandomWord() {
+  let randomWord = wordList[Math.floor(Math.random()*wordList.length)];
+  return randomWord;
+}
+
+function createLetterBoxes() {
+    for (i = 0; i < letterBoxEls.length; i++) {
+        letterBoxEls[i].appendChild()
+    }
+}
+
+// TO DO:
+  
+  // - Funktion som startar spelet vid knapptryckning, och då tillkallas andra funktioner
+    // - eventListener for button, call for selectWord()
+
+  // - Funktion som slumpar fram ett ord
+    // - wordList[Math.floor(Math.random()*wordList.length)];
+
+  // - Funktion som tar fram bokstävernas rutor, antal rutor beror på vilket ord slumptas fram
+    // - For loop? i++ osv.
+
+  // - Funktion som körs när du trycker på bokstäverna och gissar bokstav
+    // - Kolla om bokstav finns i selectedWord-array.
+    // - Om rätt: Sätt in den på rätt ruta/rutor.
+    // - Om fel: Ändra till nästa bild så gubben hängs lite mer.
+  
+  // - Funktion som ropas vid vinst eller förlust, gör olika saker beroende tillståndet
+    // - if/else if funktion.
+
+  // - Funktion som inaktiverar/aktiverar bokstavsknapparna beroende på vilken del av spelet du är på
+    // - eventListener när knappen är tryckt så inaktivera. Add attribute hidden.
