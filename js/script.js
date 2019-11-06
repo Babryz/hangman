d6a3fb4145519f6efa6740ec9fa624a059678a46
Emil Babra <brittsboemil@gmail.com>
2019-11-06 20:45:21 +0100

    Finish generateRandomWord function, now works as it should.

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 // Globala variabler
 
 const wordList = ['hangman', 'develop', 'anomaly', 'manhunt', 'human', 'dinosaur'];      // Array: med spelets alla ord
-let selectedWord;    // Sträng: ett av orden valt av en slumpgenerator från arrayen ovan
+let selectedWord = "";    // Sträng: ett av orden valt av en slumpgenerator från arrayen ovan
 
 let guesses = 0;     // Number: håller antalet gissningar som gjorts
 let hangmanImg;      // Sträng: sökväg till bild som kommer visas (och ändras) fel svar. t.ex. `/images/h1.png`
@@ -9,23 +9,35 @@ let hangmanImg;      // Sträng: sökväg till bild som kommer visas (och ändra
 let msgHolderEl = document.getElementById('message');     // DOM-nod: Ger meddelande när spelet är över
 let startGameBtnEl = document.getElementById('startGameBtn');  // DOM-nod: knappen som du startar spelet med
 let letterButtonEls = document.querySelectorAll('#letterButtons > li'); // Array av DOM-noder: Knapparna för bokstäverna
-let letterBoxEls = document.querySelectorAll('#letterBoxes > ul > li');    // Array av DOM-noder: Rutorna där bokstäverna ska stå
+let letterBoxEls = document.querySelectorAll('#letterBoxes > ul > li > input');    // Array av DOM-noder: Rutorna där bokstäverna ska stå
 
 startGameBtnEl.addEventListener('click', startGame);
 
 function startGame() {
     generateRandomWord();
+    console.log(selectedWord);
+    createLetterBoxes();
 }
 
 function generateRandomWord() {
-  let randomWord = wordList[Math.floor(Math.random()*wordList.length)];
-  console.log(randomWord);
+  let random = wordList[Math.floor(Math.random()*wordList.length)];
+  selectedWord = random
+  
 }
+
+function createLetterBoxes() {
+  for (i = 0; i < selectedWord.length; i++) {
+    letterBoxEls[i].appendChild(input);
+  };
+}
+
+
+
 
 // TO DO:
   
   // - Funktion som startar spelet vid knapptryckning, och då tillkallas andra funktioner
-    // - eventListener for button, call for selectWord()
+    // - eventListener for button, call for generateRandomWord()
 
   // - Funktion som slumpar fram ett ord
     // - wordList[Math.floor(Math.random()*wordList.length)];
